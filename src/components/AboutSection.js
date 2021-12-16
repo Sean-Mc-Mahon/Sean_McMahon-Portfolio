c2e80b1a07544a28d2c12ba493456fee55e8fc54
Sean-Mc-Mahon <sean.mcmahon87@gmail.com>
2021-12-16 12:53:14 +0000

    Global Styled Components Added

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,22 +1,59 @@
-import React from 'react'
-import profile from '../img/profile.jpg';
+import React from "react";
+import profile from "../img/profile.jpg";
+import styled from "styled-components";
 
 const aboutSection = () => {
-    return (
-        <div>
-            <div className="description">
-                <div className="title">
-                    <div className="hide">
-                        <h2>let me make</h2>
-                        <h2>your <span>dream</span> website</h2>
-                    </div>
-                </div>
-            </div>
-            <p>Contact me for any projects you have in mind.</p>
-            <button>Contact</button>
-            <img src={profile} alt="profile pic" />
+  return (
+    <About>
+      <Description>
+        <div className="title">
+          <Hide>
+            <h2>let me make</h2>
+          </Hide>
+          <Hide>
+            <h2>
+              your <span>dream</span> website
+            </h2>
+          </Hide>
         </div>
-    )
-}
+        <p>Contact me for any projects you have in mind.</p>
+        <button>Contact</button>
+      </Description>
+      <Image>
+        <img src={profile} alt="profile pic" />
+      </Image>
+    </About>
+  );
+};
 
-export default  aboutSection;
+//styled component
+const About = styled.div`
+  min-height: 90vh;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 5rem 10rem;
+  color: white;
+`;
+const Description = styled.div`
+  flex: 1;
+  padding-right: 5rem;
+  h2 {
+    font-weight: lighter;
+  }
+`;
+const Image = styled.div`
+  flex: 1;
+  overflow: hidden;
+  img {
+    width: 100%;
+    height: 80vh;
+    object-fit: cover;
+  }
+`;
+
+const Hide = styled.div`
+  overflow: hidden;
+`;
+
+export default aboutSection;
