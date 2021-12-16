f9391dd7438217c8391ef094ac5042e9df7014f2
Sean-Mc-Mahon <sean.mcmahon87@gmail.com>
2021-12-16 13:34:33 +0000

    Services Section Styled

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,6 +1,8 @@
 import React from "react";
 import profile from "../img/profile.jpg";
+//Styled
 import styled from "styled-components";
+import { About, Description, Image, Hide } from "../styles";
 
 const aboutSection = () => {
   return (
@@ -25,35 +27,5 @@ const aboutSection = () => {
     </About>
   );
 };
-
-//styled component
-const About = styled.div`
-  min-height: 90vh;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  padding: 5rem 10rem;
-  color: white;
-`;
-const Description = styled.div`
-  flex: 1;
-  padding-right: 5rem;
-  h2 {
-    font-weight: lighter;
-  }
-`;
-const Image = styled.div`
-  flex: 1;
-  overflow: hidden;
-  img {
-    width: 100%;
-    height: 80vh;
-    object-fit: cover;
-  }
-`;
-
-const Hide = styled.div`
-  overflow: hidden;
-`;
 
 export default aboutSection;
--- src/components/ServicesSection.js
+++ src/components/ServicesSection.js
@@ -1,30 +1,33 @@
 import React from "react";
 //import Images
 import me3 from "../img/me3.jpg";
+//Styled
+import styled from "styled-components";
+import { About, Description, Image, Hide } from "../styles";
 
 function ServicesSection() {
   return (
-    <div className="services">
-      <div className="description">
+    <Services>
+      <Description>
         <h2>
           Clean and <span>beautiful</span> UX design
         </h2>
-        <div className="cards">
-          <div className="card">
+        <Cards>
+          <Card>
             <div className="icon"></div>
             <p>
               Lorem ipsum dolor sit amet consectetur adipisicing elit.
               Architecto, pariatur!
             </p>
-          </div>
-          <div className="card">
+          </Card>
+          <Card>
             <div className="icon"></div>
             <p>
               Lorem ipsum dolor sit amet consectetur adipisicing elit.
               Architecto, pariatur!
             </p>
-          </div>
-          <div className="card">
+          </Card>
+          <Card>
             <div className="icon">
               <h3>Teamwork</h3>
             </div>
@@ -32,8 +35,8 @@ function ServicesSection() {
               Lorem ipsum dolor sit amet consectetur adipisicing elit.
               Architecto, pariatur!
             </p>
-          </div>
-          <div className="card">
+          </Card>
+          <Card>
             <div className="icon">
               <h3>Teamwork</h3>
             </div>
@@ -41,8 +44,8 @@ function ServicesSection() {
               Lorem ipsum dolor sit amet consectetur adipisicing elit.
               Architecto, pariatur!
             </p>
-          </div>
-          <div className="card">
+          </Card>
+          <Card>
             <div className="icon">
               <h3>Teamwork</h3>
             </div>
@@ -50,8 +53,8 @@ function ServicesSection() {
               Lorem ipsum dolor sit amet consectetur adipisicing elit.
               Architecto, pariatur!
             </p>
-          </div>
-          <div className="card">
+          </Card>
+          <Card>
             <div className="icon">
               <h3>Teamwork</h3>
             </div>
@@ -59,12 +62,42 @@ function ServicesSection() {
               Lorem ipsum dolor sit amet consectetur adipisicing elit.
               Architecto, pariatur!
             </p>
-          </div>
-        </div>
-      </div>
-      <img src={me3} alt="3 of me" />
-    </div>
+          </Card>
+        </Cards>
+      </Description>
+      <Image>
+        <img src={me3} alt="3 of me" />
+      </Image>
+    </Services>
   );
 }
+
+const Services = styled(About)`
+  h2 {
+    padding-bottom: 5rem;
+  }
+  p {
+    width: 70%;
+    padding: 2rem 0rem 4rem 0rem;
+  }
+`;
+
+const Cards = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+`;
+const Card = styled.div`
+  flex-basis: 20rem;
+  .icon {
+    display: flex;
+    align-items: center;
+    h3 {
+      margin-left: 1rem;
+      background: white;
+      color: black;
+      padding: 1rem;
+    }
+  }
+`;
 
 export default ServicesSection;
